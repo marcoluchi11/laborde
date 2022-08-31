5ca6ae0a8e0c9d01aeb141d0ab2f20465ea514d2
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-31 17:38:23 -0300

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import LabordeProvider from "./context/LabordeContext";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <LabordeProvider>
+      <Router>
+        <App />
+      </Router>
+    </LabordeProvider>
   </React.StrictMode>
 );
