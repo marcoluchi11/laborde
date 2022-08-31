2ab5df5adeffc72128a4368b4963fe87998ea395
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-31 17:38:11 -0300

    add home component

---
+++ src/components/User/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Search from "./Search";
+import Cards from "./Cards";
+import Contact from "./Contact";
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Search />
+      <Cards />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
