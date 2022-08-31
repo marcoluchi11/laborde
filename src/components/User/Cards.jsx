b71f0de95c72de7cc75900df44cfd007529a6752
marcoluchi11 <marcoaluchi@hotmail.com>
2022-08-31 17:40:04 -0300

    add flexwrap in styles

--- src/components/User/Cards.jsx
+++ src/components/User/Cards.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
   justify-content: space-around;
+  flex-wrap: wrap;
 `;
 
 const Card = styled.div`
@@ -14,54 +15,58 @@ const Card = styled.div`
 
 const Cards = () => {
   return (
-    <Container>
-      <Card>
-        <img
-          src="https://picsum.photos/200
-"
-          alt="holis"
-        />
-        <h3>Libro nuevo</h3>
-        <p>1300$</p>
-      </Card>
-      <Card>
-        <img
-          src="https://picsum.photos/200
-"
-          alt="holis"
-        />
-        <h3>Libro nuevo</h3>
-        <p>1300$</p>
-      </Card>
-      <Card>
-        <img
-          src="https://picsum.photos/200
-"
-          alt="holis"
-        />
+    <div>
+      <h1>Los mas buscados</h1>
 
-        <h3>Libro nuevo</h3>
-        <p>1300$</p>
-      </Card>
-      <Card>
-        <img
-          src="https://picsum.photos/200
+      <Container>
+        <Card>
+          <img
+            src="https://picsum.photos/200
 "
-          alt="holis"
-        />
-        <h3>Libro nuevo</h3>
-        <p>1300$</p>
-      </Card>
-      <Card>
-        <img
-          src="https://picsum.photos/200
+            alt="holis"
+          />
+          <h3>Libro nuevo</h3>
+          <p>1300$</p>
+        </Card>
+        <Card>
+          <img
+            src="https://picsum.photos/200
 "
-          alt="holis"
-        />
-        <h3>Libro nuevo</h3>
-        <p>1300$</p>
-      </Card>
-    </Container>
+            alt="holis"
+          />
+          <h3>Libro nuevo</h3>
+          <p>1300$</p>
+        </Card>
+        <Card>
+          <img
+            src="https://picsum.photos/200
+"
+            alt="holis"
+          />
+
+          <h3>Libro nuevo</h3>
+          <p>1300$</p>
+        </Card>
+        <Card>
+          <img
+            src="https://picsum.photos/200
+"
+            alt="holis"
+          />
+          <h3>Libro nuevo</h3>
+          <p>1300$</p>
+        </Card>
+        <Card>
+          <img
+            src="https://picsum.photos/200
+"
+            alt="holis"
+          />
+          <h3>Libro nuevo</h3>
+          <p>1300$</p>
+        </Card>
+      </Container>
+    </div>
   );
 };
 
