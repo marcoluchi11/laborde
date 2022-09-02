89db88129aac21763379a1fd748ab867597f4fcb
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-02 12:16:53 -0300

    add cart state

--- src/context/LabordeContext.jsx
+++ src/context/LabordeContext.jsx
@@ -3,9 +3,9 @@ export const LabordeContext = createContext();
 
 const LabordeProvider = ({ children }) => {
   const [search, setSearch] = useState("");
-
+  const [cart, setCart] = useState([]);
   return (
-    <LabordeContext.Provider value={{ search, setSearch }}>
+    <LabordeContext.Provider value={{ search, setSearch, cart, setCart }}>
       {children}
     </LabordeContext.Provider>
   );
