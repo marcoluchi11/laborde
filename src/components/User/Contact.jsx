6ec8fc349ed4856227586e16fb944c42a6e6ce6d
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-02 12:15:42 -0300

    add styles to contact

--- src/components/User/Contact.jsx
+++ src/components/User/Contact.jsx
@@ -1,21 +1,36 @@
 import styled from "styled-components";
+import ContactForm from "./ContactForm";
+import Map from "./Map";
 
 const Container = styled.section`
   background-color: #444444;
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
   color: #fff;
   margin: 1rem;
   padding: 1rem;
   border-radius: 15px;
+  div {
+    width: 50%;
+  }
 `;
 const Contact = () => {
   return (
     <Container>
-      <h1>Direccion</h1>
-      <p> 3 de febrero 1065,Rosario</p>
-      <h1>Telefono</h1>
-      <p> +54 3416692059</p>
-      <h1> Email</h1>
-      <p> asdasd@yahoo.com</p>
+      <div>
+        <h1>Dirección</h1>
+        <p> 3 de Febrero 1065</p>
+        <p>Rosario, Santa Fe, Argentina</p>
+        <h1>Teléfono</h1>
+        <p> +54 3416692059</p>
+        <h1> Email</h1>
+        <p> asdasd@yahoo.com</p>
+        <Map />
+      </div>
+      <div>
+        <ContactForm />
+      </div>
     </Container>
   );
 };
