96be6bf66d3176b0d1b8c6dca0c8c6a231aa537c
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-02 12:17:45 -0300

    add cart to routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/User/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/User/Home";
 import Who from "./components/User/Who";
+import Cart from "./components/User/Cart";
 const App = () => {
   return (
     <>
@@ -12,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/who" element={<Who />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </main>
       <Footer />
