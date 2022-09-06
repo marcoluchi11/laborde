d83570f8e035eddc77093a4c2d8ee96ae6bb1d71
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-06 17:54:16 -0300

    separate payment from principal component

--- src/components/User/Cart/Cart.jsx
+++ src/components/User/Cart/Cart.jsx
@@ -2,6 +2,7 @@ import { nanoid } from "nanoid";
 import { useContext } from "react";
 import styled from "styled-components";
 import { LabordeContext } from "../../../context/LabordeContext";
+import Payment from "./Payment";
 const Container = styled.section`
   background-color: #fff;
   margin: 0 1rem;
@@ -14,17 +15,36 @@ const Container = styled.section`
 const Form = styled.form`
   display: flex;
   flex-direction: column;
+  width: 100%;
 `;
 const Item = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-evenly;
   align-items: center;
   width: 100%;
+  margin-bottom: 2rem;
+  border-bottom: 1px solid #e6e6e6;
+  @media (max-width: 720px) {
+    h3 {
+      font-size: 0.9rem;
+    }
+    h2 {
+      font-size: 1rem;
+    }
+    .isbn {
+      display: none;
+    }
+  }
   div {
     display: flex;
     flex-direction: column;
     text-align: center;
     height: 100%;
+    width: 15rem;
+    height: 11rem;
+  }
+  div.image {
+    align-items: center;
   }
   img {
     border-radius: 50%;
@@ -34,6 +54,9 @@ const Item = styled.div`
   h3 {
     margin-bottom: 1rem;
     padding: 0;
+  }
+  h3.price {
+    color: #333;
   }
 `;
 const NoProducts = styled.section`
@@ -47,24 +70,7 @@ const NoProducts = styled.section`
     padding: 1rem;
   }
 `;
-const Pagar = styled.button`
-  outline: 0;
-  border: 0;
-  display: flex;
-  justify-content: flex-end;
-  cursor: pointer;
-  margin: 1rem;
-  padding: 0.5rem;
-  background-color: limegreen;
-  color: #fff;
-  border-radius: 5px;
-  transition: 1s background-color ease;
-  transition: 1s color ease;
-  &&:hover {
-    background-color: #fff;
-    color: limegreen;
-  }
-`;
+
 const Cart = () => {
   const { cart } = useContext(LabordeContext);
   if (cart.length === 0)
@@ -78,29 +84,27 @@ const Cart = () => {
       <h1>Carrito</h1>
       <Form method="GET" action="http://localhost:4000/payment">
         <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-        {cart.map((elem) => (
+        {cart.map((elem, index) => (
           <Item key={nanoid()}>
-            <div>
-              <h3>Imagen</h3>
+            <div className="image">
+              <h3>Item #{index + 1}</h3>
               <img src={elem.image} alt="imagen" />
             </div>
             <div>
-              <h3>Titulo</h3>
+              <h3>Título</h3>
               <h3>{elem.title}</h3>
             </div>
-            <div>
-              <h3>Codigo ISBN</h3>
-              <h3>Codigo ISBN:{elem.isbn}</h3>
+            <div className="isbn">
+              <h3>Código ISBN</h3>
+              <h3>{elem.isbn}</h3>
             </div>
             <div>
-              <h3>Precio</h3>
+              <h3 className="price">Precio</h3>
               <h2>{elem.price}</h2>
             </div>
           </Item>
         ))}
-        <Pagar formTarget="_blank" type="submit">
-          PAGAR
-        </Pagar>
+        <Payment />
       </Form>
     </Container>
   );
