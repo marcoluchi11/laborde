b2aaa4dc0d7f03bad88f714aaeab9e4bb71e1ab7
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-06 17:53:43 -0300

    add total state

--- src/context/LabordeContext.jsx
+++ src/context/LabordeContext.jsx
@@ -4,8 +4,11 @@ export const LabordeContext = createContext();
 const LabordeProvider = ({ children }) => {
   const [search, setSearch] = useState("");
   const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
   return (
-    <LabordeContext.Provider value={{ search, setSearch, cart, setCart }}>
+    <LabordeContext.Provider
+      value={{ search, setSearch, cart, setCart, total, setTotal }}
+    >
       {children}
     </LabordeContext.Provider>
   );
