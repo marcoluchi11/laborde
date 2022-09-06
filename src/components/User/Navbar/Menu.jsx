5e960ae08ac9d40a9ae4cec676897674001bb87b
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-06 16:30:37 -0300

    add style to links, add number to length in cart

--- src/components/User/Navbar/Menu.jsx
+++ src/components/User/Navbar/Menu.jsx
@@ -1,35 +1,48 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { LabordeContext } from "../../../context/LabordeContext";
 
 const Lista = styled.ul`
+  @media (min-width: 720px) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+    width: 50%;
+  }
   display: flex;
-  align-items: center;
+  flex-direction: column;
+  font-weight: 300;
   color: #000;
   list-style-type: none;
   margin-right: 5rem;
   a {
     text-decoration: none;
+    &:visited {
+      color: inherit;
+    }
   }
 
   li {
     margin: 0.5rem;
-    font-weight: 600;
+
     cursor: pointer;
   }
 `;
 const Menu = () => {
+  const { cart } = useContext(LabordeContext);
   return (
     <Lista>
       <Link to="/">
         <li>Inicio</li>
       </Link>
-
       <Link to="/who">
         <li>Quienes Somos</li>
       </Link>
       <li>Contacto</li>
       <Link to="/cart">
-        <li>Carrito</li>
+        <li>Carrito ({cart.length})</li>
       </Link>
     </Lista>
   );
