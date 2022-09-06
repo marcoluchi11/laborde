bd2e12cc78fc567f26e69084cf73e1150b9f1ce5
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-06 16:31:44 -0300

    add styles to cards

--- src/components/User/Cards.jsx
+++ src/components/User/Cards.jsx
@@ -6,19 +6,58 @@ import data from "./../../bookdata.json";
 
 const Container = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   flex-wrap: wrap;
 `;
-
 const Card = styled.div`
-  width: 25%;
   margin: 1rem;
-  border: 1px solid black;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 5px;
+  box-shadow: 10px 10px 5px -10px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 10px 10px 5px -10px rgba(0, 0, 0, 0.75);
+  -moz-box-shadow: 10px 10px 5px -10px rgba(0, 0, 0, 0.75);
   img {
     cursor: pointer;
+    max-width: 100%;
+    max-height: 100%;
+  }
+  h3 {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 160px;
+    height: 1.2em;
+    white-space: nowrap;
+  }
+  p {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    width: 160px;
+    height: 1.2em;
+    white-space: nowrap;
+    font-weight: 100;
   }
 `;
-
+const DivButton = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+const DivTitle = styled.div``;
+const Agregar = styled.button`
+  outline: none;
+  border: none;
+  background-color: #26b16d;
+  color: #fff;
+  border-radius: 10px;
+  margin: 1rem 0;
+  padding: 0.5rem;
+  cursor: pointer;
+`;
+const Title = styled.h1`
+  margin: 0;
+  padding: 0;
+  text-align: center;
+`;
 const Cards = () => {
   const { setCart, cart } = useContext(LabordeContext);
   const handleCart = (item) => {
@@ -26,20 +65,28 @@ const Cards = () => {
   };
   return (
     <div>
-      <h1>Los mas buscados</h1>
-
+      <div>
+        <Title>Los mas buscados</Title>
+      </div>
       <Container>
         {data.map((elem) => (
           <Card key={nanoid()}>
-            <img
-              onClick={() => handleCart(elem)}
-              src="https://picsum.photos/200
-  "
-              alt="holis"
-            />
+            <div>
+              <img src={elem.image} alt="holis" />
+            </div>
             <h3>{elem.title}</h3>
-            <small>{elem.isbn}</small>
-            <p>{elem.price}</p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,
+              reprehenderit tempora velit exercitationem deserunt perferendis
+              facilis eveniet commodi eligendi impedit!
+            </p>
+            <small>ISBN: {elem.isbn}</small>
+            <h4>{elem.price}</h4>
+            <DivButton>
+              <Agregar onClick={() => handleCart(elem)}>
+                Agregar al carrito
+              </Agregar>
+            </DivButton>
           </Card>
         ))}
       </Container>
@@ -48,3 +95,8 @@ const Cards = () => {
 };
 
 export default Cards;
+// DISEÑAR CARDS
+// ACOMODAR CONTACT
+// MEJORAR FOOTER
+// MAS LINDO <NAVBAR
+// QUE SEA TODO BIEN RESPONSIVE
