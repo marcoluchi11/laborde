097e968a2aea60e29fe9b2f558c3e8fb660b9d1e
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-13 16:37:47 -0300

    add books and filterbooks for search feature

--- src/context/LabordeContext.jsx
+++ src/context/LabordeContext.jsx
@@ -1,13 +1,27 @@
 import { createContext, useState } from "react";
+import list from "./../bookdata.json";
 export const LabordeContext = createContext();
 
 const LabordeProvider = ({ children }) => {
   const [search, setSearch] = useState("");
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+  const [books, setBooks] = useState(list);
+  const [filterBooks, setFilterBooks] = useState([]);
   return (
     <LabordeContext.Provider
-      value={{ search, setSearch, cart, setCart, total, setTotal }}
+      value={{
+        search,
+        setSearch,
+        cart,
+        setCart,
+        total,
+        setTotal,
+        books,
+        setBooks,
+        filterBooks,
+        setFilterBooks,
+      }}
     >
       {children}
     </LabordeContext.Provider>
