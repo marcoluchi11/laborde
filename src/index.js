dc501a128e0aba2dfc2ba71414e03671dc56ea54
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-16 17:22:38 -0300

    add route for admin

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import LabordeProvider from "./context/LabordeContext";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Admin/Home";
+import AddBook from "./components/Admin/AddBook";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <LabordeProvider>
       <Router>
-        <App />
+        <Routes>
+          <Route path="/admin" element={<Home />} />
+          <Route path="/admin/add" element={<AddBook />} />
+          <Route path="/" element={<App />} />
+        </Routes>
       </Router>
     </LabordeProvider>
   </React.StrictMode>
