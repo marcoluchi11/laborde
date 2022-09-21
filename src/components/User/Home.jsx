bff00c87d32a3d3168e735b72aaf171afd800611
marcoluchi11 <marcoaluchi@hotmail.com>
2022-09-21 15:36:18 -0300

    add useEffect when mounting component

--- src/components/User/Home.jsx
+++ src/components/User/Home.jsx
@@ -2,7 +2,15 @@ import Header from "./Header";
 import Search from "./Search";
 import Cards from "./Cards";
 import Contact from "./Contact";
+import { useEffect } from "react";
+import { useContext } from "react";
+import { LabordeContext } from "../../context/LabordeContext";
 const Home = () => {
+  const { getBooks } = useContext(LabordeContext);
+  useEffect(() => {
+    getBooks();
+    //eslint-disable-next-line
+  }, []);
   return (
     <div>
       <Header />
