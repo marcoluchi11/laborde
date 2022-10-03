6ec26ee92ccfdbe6fc63def769995dd53cbe20f5
marcoluchi11 <marcoaluchi@hotmail.com>
2022-10-03 18:45:56 -0300

    add same parts as original cards

--- src/components/User/FilterCards.jsx
+++ src/components/User/FilterCards.jsx
@@ -1,20 +1,24 @@
 import { nanoid } from "nanoid";
+import { useContext } from "react";
+import { LabordeContext } from "../../context/LabordeContext";
+import { getCategory } from "../helpers";
 import { Agregar, DivButton, Item } from "./Card";
 
 const FilterCards = ({ elem, handleCart }) => {
+  const { setCardDetail } = useContext(LabordeContext);
+  const handleClick = (elem) => {
+    setCardDetail(elem);
+  };
   return (
     <Item key={nanoid()}>
       <div>
-        <img src={elem.image} alt="holis" />
+        <img onClick={() => handleClick(elem)} src={elem.image} alt="holis" />
       </div>
-      <h3>{elem.title}</h3>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla,
-        reprehenderit tempora velit exercitationem deserunt perferendis facilis
-        eveniet commodi eligendi impedit!
-      </p>
+      <small className="category">{getCategory(elem.category)}</small>
+      <h3 onClick={() => handleClick(elem)}>{elem.title}</h3>
+      <h5>{elem.author.toUpperCase()}</h5>
       <small>ISBN: {elem.isbn}</small>
-      <h4>${elem.price}</h4>
+      <h4>${elem.price.toFixed(2)}</h4>
       <DivButton>
         <Agregar onClick={() => handleCart(elem)}>Agregar al carrito</Agregar>
       </DivButton>
