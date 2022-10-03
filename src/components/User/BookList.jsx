f07eb90260b36eb9638c36688b7249f54a5973c8
marcoluchi11 <marcoaluchi@hotmail.com>
2022-10-03 18:45:15 -0300

    add booklist component to separate filter cards and cards from card detail

---
+++ src/components/User/BookList.jsx
@@ -0,0 +1,19 @@
+import { nanoid } from "nanoid";
+import { useContext } from "react";
+import CardDetail from "./CardDetail";
+import { LabordeContext } from "../../context/LabordeContext";
+import Card from "./Card";
+import FilterCards from "./FilterCards";
+const BookList = ({ handleCart }) => {
+  const { filterBooks, booklist, cardDetail } = useContext(LabordeContext);
+  if (cardDetail) return <CardDetail />;
+  return filterBooks.length === 0
+    ? booklist.map((elem) => (
+        <Card key={nanoid()} elem={elem} handleCart={handleCart} />
+      ))
+    : filterBooks.map((elem) => (
+        <FilterCards key={nanoid()} elem={elem} handleCart={handleCart} />
+      ));
+};
+
+export default BookList;
