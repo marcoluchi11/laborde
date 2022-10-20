92ac22ffdf47d365bedb014cd713fd997d99f1b7
marcoluchi11 <marcoaluchi@hotmail.com>
2022-10-20 11:42:24 -0300

    erase return of carddetail component

--- src/components/User/BookList.jsx
+++ src/components/User/BookList.jsx
@@ -1,12 +1,10 @@
 import { nanoid } from "nanoid";
 import { useContext } from "react";
-import CardDetail from "../User/Cards/CardDetail";
 import { LabordeContext } from "../../context/LabordeContext";
 import Card from "../User/Cards/Card";
 import FilterCards from "./FilterCards";
 const BookList = ({ handleCart }) => {
-  const { filterBooks, booklist, cardDetail } = useContext(LabordeContext);
-  if (cardDetail) return <CardDetail />;
+  const { filterBooks, booklist } = useContext(LabordeContext);
   return filterBooks.length === 0
     ? booklist.map((elem) => (
         <Card key={nanoid()} elem={elem} handleCart={handleCart} />
